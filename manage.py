53d344abc0ffaec1baa712fc43eeead5be55013d
Aleksei Pokrevskiy <apokrevskiy@ipressroom.com>
2010-12-26 21:21:03 +0300

    initial comit

---
+++ manage.py
@@ -0,0 +1,16 @@
+from werkzeug.debug import DebuggedApplication
+from werkzeug import run_simple
+from blok.application import Blok
+import os
+from werkzeug.wsgi import SharedDataMiddleware
+
+application = Blok()
+#application = DebuggedApplication(application, evalex=True)
+application = SharedDataMiddleware(application, {
+    '/static':     os.path.join(os.getcwd(), 'blok/static')
+})
+run_simple('localhost', 4000, application, use_reloader=True)
+
+
+
+  
